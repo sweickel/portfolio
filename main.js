a121c6d5fe5f7c4e2b045c04880367e3e311ebec
Sean Weickel <sweickel@gmail.com>
2021-02-23 00:14:37 -0500

    added scroll to top button

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("hi");
+
+
+
+window.addEventListener("load", (event)=>{
+    let top = document.getElementById("top");
+    window.addEventListener("scroll", (event)=>{
+    
+        top.style.display = "flex";
+        
+    });
+
+    top.addEventListener("click", (event)=>{
+        window.scrollTo(0, 0);
+    });
+});
+
+
+
