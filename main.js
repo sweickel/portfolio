47b41a4858b4a62aa0e1bd96ade443cb703ff21e
Sean Weickel <sweickel@gmail.com>
2021-02-24 22:56:51 -0500

    finished all sections, all styling, all function, all links

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ console.log("hi");
 
 window.addEventListener("load", (event)=>{
     let top = document.getElementById("top");
-    window.addEventListener("scroll", (event)=>{
-    
-        top.style.display = "flex";
-        
+    window.addEventListener("scroll", (event)=>{    
+        top.style.display = "flex";    
     });
 
     top.addEventListener("click", (event)=>{
-        window.scrollTo(0, 0);
+        window.scrollTo({
+           top: 0,
+           left: 0,
+           behavior: 'smooth'
+        });
     });
 });
 
